b2d93703324cdf1b7b5d98a594ebcaa4c3667bc0
Aleksandar1601 <pajicaleksandar44@gmail.com>
2018-11-13 10:31:20 +0100

    Online Prodaja guma
    Online prodaja guma

---
+++ prodajaguma/php/footer.php
@@ -0,0 +1,28 @@
+<?php echo'<!-- /.container -->
+<footer id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 footer-leftcol">
+                <p><span class="bold-16p">Visoka tehnička škola strukovnih studija</span><br>
+                    Segedinski put 9<br>
+                    Subotica, 24000 Srbija - <a href="#">aleksandarpajic44@vts.su.ac.rs</a>
+                </p>
+            </div>
+            <div class="col-md-4 footer-midcol">
+                <ul>
+                    <li><a href="#" class="twitter-icon">Twitter</a></li>
+                    <li><a href="#" class="facebook-icon">Facebook</a></li>
+                    <li><a href="#" class="pintrest-icon">Pintrest</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-4 footer-rightcol">
+                <p>Designed by <a href="#"><img src="img/yebo-icon.png" alt="Alex Logo"></a></p>
+                <p class="nopadding">Developed by <a href="http://skolabalkan.000webhostapp.com">Aleksandar Pajic</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>';
+?>
---
+++ prodajaguma/php/header.php
@@ -0,0 +1,56 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Gume prodaja online besplatna dostava uštedi kupi zimske letnje Firelli">
+    <meta name="description" content="Online prodaja guma za sve vrste vozila.">
+    <meta name="author" content="Online prodaja guma">
+    <title>Online prodaja guma / Prodavnica</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css?v=1.2" rel="stylesheet">
+    <link href="css/queries.css?v=1.2" rel="stylesheet">
+    <link href="img/icon.png" rel="icon">
+    <link rel="stylesheet" href="css/flexslider.css?v=1.2" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+<body>
+<header>
+    <div class="container" >
+        <div class="row">
+            <div class="responsive-logo"></div>
+            <div class="pullcontainer">
+                <a href="#" id="pull"><i class="fa fa-bars fa-2x"></i></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <nav>
+                    <div class="logo-holder"></div>
+                    <ul class="clearfix">
+                        <li><a href="about.html">O Nama</a></li>
+                        <li class="dot">.</li>
+                        <li><a href="index.html" class="r_spacer">Početna</a></li>
+                        <li><a href="shop.html">Radnja</a></li>
+                        <li class="dot">.</li>
+                        <li><a href="contact.html">Kontakt</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="hero"></div>
+</header>
+
+';
+?>
